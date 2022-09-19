1838ce620346349deb66f6cc3fae2291990cc420
Nickolay Mitrofanov <nick@local>
2022-09-19 20:28:22 +0300

    ++

--- src/copy_to_flash/gui/approve_list_dialog.py
+++ src/copy_to_flash/gui/approve_list_dialog.py
@@ -1,4 +1,4 @@
-from tkinter import BOTH, BOTTOM, END, HORIZONTAL, LEFT, RIGHT, TOP, VERTICAL, X, Y, Frame, Label, Listbox, Scrollbar, Toplevel
+from tkinter import BOTH, END, HORIZONTAL, LEFT, RIGHT, TOP, VERTICAL, X, Y, Frame, Label, Listbox, Scrollbar, simpledialog
 from typing import List
 
 from copier import SourceFile
@@ -23,25 +23,30 @@ class ListAdapter(object):
             raise ValueError("Incorrect index")
         del self.list[i]
 
-class ApproveRemoveBeforeDialog(Toplevel):
+class ApproveRemoveBeforeDialog(simpledialog.Dialog):
     def __init__(self, master, list: ListAdapter) -> None:
-        super().__init__(master)
+        self.__list = list
+        super().__init__(master, title='Clear destination before copy')
 
+        
+    def body(self, master: Frame):
         self._answer_lbl = Label(master=self, text="Do you want remove next files from flash device before copy?")
+        self._answer_lbl.configure(compound='left')
 
         self.__list_frame_full = Frame(self)
         self.__list_frame_with_x = Frame(self.__list_frame_full)
 
-        self.__scrollbar_y = Scrollbar(self.__list_frame_full, orient=VERTICAL)
+        self.__scrollbar_y = Scrollbar(self.__list_frame_full, orient=VERTICAL, width=10)
         self.__scrollbar_x = Scrollbar(self.__list_frame_with_x, orient=HORIZONTAL)
 
         self.__listbox = Listbox(
             master=self.__list_frame_with_x,
             xscrollcommand=self.__scrollbar_x.set, 
-            yscrollcommand=self.__scrollbar_y.set, 
-            width=40,
+            yscrollcommand=self.__scrollbar_y.set,
+            width=80,
+            height=22,
         )
-        for f in list.list:
+        for f in self.__list.list:
             self.__listbox.insert(END, f.path)
 
         self.__scrollbar_x.configure(command=self.__listbox.xview)
@@ -57,4 +62,5 @@ class ApproveRemoveBeforeDialog(Toplevel):
         self.__list_frame_with_x.pack(side=LEFT, fill=BOTH, expand=True)
         self.__scrollbar_y.pack(side=RIGHT, fill=Y, expand=True)
 
-        
+        self.geometry("700x500")
+        return super().body(master)
